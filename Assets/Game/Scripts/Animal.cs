280a695153a25b6db9146208741f2c5ba1c0bf5b
Darren Alain Pagdonsolan Manalo <darrenapm1016@gmail.com>
2024-03-24 22:40:12 -0700

    IDK, i'm trynna pull it

---
+++ Assets/Game/Scripts/Animal.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+  public float speed = 3.0f;  // How fast the animal is.
+
+  public Vector2 velocity; //Connects to Tower math
+  public int damage = 1;      // Amount of damage done when animal gets through.
+  public int health = 1;      // How tanky the animal is.
+  public int value;           // Amount of money to give when dead.
+  private int _waypointIndex;  // Index of the waypoint it is going towards.
+
+
+  private void Update() {
+    Vector2 nextPosition = getNextPosition();
+    velocity = ((Vector2)transform.position * GameData.instance.track.getWaypointPosition(_waypointIndex)) * speed;
+    // Readjust rotation of the Sprite.
+    Vector2 direction = (Vector2)gameObject.transform.position - nextPosition;
+    float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+    transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    if (direction.x < 0) {  // Going right, flip Y to make it upright.
+      GetComponent<SpriteRenderer>().flipY = true;
+    } else {  // Going left, unflip.
+      GetComponent<SpriteRenderer>().flipY = false;
+    }
+
+    // Move animal to next position.
+    transform.position = nextPosition;
+
+    // Reached a waypoint, move to next waypoint.
+    if (Vector2.Distance(transform.position, GameData.instance.track.getWaypointPosition(_waypointIndex)) < 0.002f) {
+      ++_waypointIndex;
+      
+      // Reached the end of the track..
+      if (_waypointIndex >= GameData.instance.track.waypoints.Length) {
+        Destroy(gameObject);
+      }
+    }
+  }
+
+/**
+  * gets remaining distance from animal to endpoint
+  */
+  public float GetRemainingDistance(){
+    return GameData.instance.track.getCumulativeDist(_waypointIndex, transform.position);
+  }
+
+  /**
+   * Calculates the next position to go to.
+   *
+   * @return Vector2 - the next position.
+   */
+  private Vector2 getNextPosition() {
+    Vector2 nextPosition = GameData.instance.track.getWaypointPosition(_waypointIndex);
+    Vector2 direction = nextPosition - (Vector2)transform.position;
+
+    return (Vector2)transform.position + speed * Time.deltaTime * direction.normalized;
+  }
+}
--- Assets/Game/Scripts/Track.cs
+++ Assets/Game/Scripts/Track.cs
@@ -4,23 +4,38 @@ using UnityEngine;
 
 public class Track : MonoBehaviour
 {
-  public Transform[] waypoints;
+  public Transform[] waypoints;  // Depicts the pathing of the animals.
 
-  // Start is called before the first frame update
-  void Start() {
+  public float getCumulativeDist(int index, Vector2 pos){ //distance from waypoints + position of tower
+    float distance = Vector2.Distance(pos, waypoints[index].position);
+    for (int i = index; i < waypoints.Length - 1; i++){
+      distance += Vector2.Distance(waypoints[i].position, waypoints[i+1].position);
+    }
+    return distance;
   }
 
-  // Update is called once per frame
-  void Update() {
-  }
-
+  /**
+   * Draw circular overlay for waypoints and connection lines toward waypoints.
+   */
   private void OnDrawGizmos() {
+    // For every waypoint.
     for (int i = 0; i < waypoints.Length; ++i) {
+      // Circular overlay.
       Gizmos.color = Color.cyan;
-      Gizmos.DrawSphere(waypoints[i].position, 0.2f);
+      Gizmos.DrawSphere(waypoints[i].position, 0.15f);
+
+      // Connection lines.
       if (i > 0) {
         Gizmos.DrawLine(waypoints[i - 1].position, waypoints[i].position);
       }
     }
+  }
+
+
+  /**
+   * Retrieve a waypoint's position.
+   */
+  public Vector2 getWaypointPosition(int index) {
+    return waypoints[index].position;
   }
 }
