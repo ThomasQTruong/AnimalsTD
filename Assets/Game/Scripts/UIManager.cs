f258f7f53496ac03561daf8e9c000fb659c50c36
truong, thomas <truongthomasq@gmail.com>
2024-03-28 14:15:48 -0700

    Documented code; fixed code to fit style; hid upgrade menu.

--- Assets/Game/Scripts/UIManager.cs
+++ Assets/Game/Scripts/UIManager.cs
@@ -3,65 +3,84 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class UIManager : MonoBehaviour
-{
-    public static UIManager instance;
-    public TMP_Text moneyText;
-    public TMP_Text healthText;
-    public TMP_Text roundText;
+public class UIManager : MonoBehaviour {
+  public static UIManager instance;
+  public GameObject towerMenu;
+  public GameObject upgradeMenu;
 
-    public GameObject towerMenu;
-    public GameObject upgradeMenu;
+  // Info UI texts.
+  public TMP_Text moneyText;
+  public TMP_Text healthText;
+  public TMP_Text roundText;
+
+  private Tower selectedTower;
 
 
-    private Tower selectedTower;
+  /**
+   * Sells the tower that is selected.
+   */
+  public void SellSelectedTower() {
+    GameData.instance.money += selectedTower.price / 2;
+    Destroy(selectedTower.gameObject);
+    DeselectTower();
+  }
 
-    public void sellSelectedTower(){
-        GameData.instance.money += selectedTower.price / 2;
-        Destroy(selectedTower.gameObject);
-        deselectTower();
+
+  /**
+   * Selects a tower; menu to buy turret disappears and upgrade menu appears.
+   * 
+   * @param tower - the tower that is selected.
+   */
+  public void SelectTower(Tower tower){
+    selectedTower = tower;
+    towerMenu.SetActive(false);
+    upgradeMenu.SetActive(true);
+  }
+
+
+  /**
+   * Unselects a tower; makes upgrade menu disappear and tower menu reappear.
+   */
+  public void DeselectTower() {
+    towerMenu.SetActive(true);
+    upgradeMenu.SetActive(false);
+  }
+
+
+  /**
+   * Updates the Health, Money, and Round info for the UI.
+   */
+  public void UpdateInfoUI() {
+    healthText.text = "Health: " + GameData.instance.health;
+    moneyText.text = "Money: " + GameData.instance.money;
+    roundText.text = "Round: " + GameData.instance.currentRound;
+  }
+
+
+  /**
+   * Updates the Info UI every frame.
+   */
+  void Update() {
+    UpdateInfoUI();
+  }
+
+
+  /**
+   * Limits object to one instance.
+   */
+  private void Awake() {
+    instance = this;
+  }
+
+
+  // Fusion menu for combinable towers to be implemented in the future if time allows it.
+  /*
+  public void fuseTower(Tower tower1, Tower tower2) {
+    if(tower1 + tower2 == fusionTowerID) {
+      tower1 = fusionTowerID;
+      // or something like that
     }
-
-    //After selecting a tower, menu to buy turret disappears and upgrade menu appears
-    public void selectTower(Tower t){
-        selectedTower = t;
-        towerMenu.SetActive(false);
-        upgradeMenu.SetActive(true);
-    }
-
-    //Clicking off of a tower will make upgrade menu disappear and tower menu reappear
-    public void deselectTower(){
-        towerMenu.SetActive(true);
-        upgradeMenu.SetActive(false);
-    }
-
-    //Fusion menu for combinable towers to be implemented in the future if time allows i
-    /*
-    public void fuseTower(Tower t1, Tower t2)
-    {
-        if(t1 + t2 == fusionTowerID)
-        {
-            t1 = fusionTowerID;
-            // or something like that
-        }
-
-    }
-    or something similar
-    */
-
-    public void updateTextUi(){
-        healthText.text = "Health: " + GameData.instance.health;
-        moneyText.text = "Money: " + GameData.instance.money;
-        roundText.text = "Round: " + GameData.instance.currentRound;
-    }
-
-    // Update is called once per frame
-    void Update(){
-        updateTextUi();
-    }
-
-    //Sometimes might display instance doesn't exist in this context
-    private void Awake(){
-        instance = this;
-    }
+  }
+  or something similar
+  */
 }
